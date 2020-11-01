d382e48e098d62c6b06292863fea912820e1cbb4
primandikakiki <primandikakiki@gmail.com>
2020-11-01 12:02:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarrecycleview/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.belajarrecycleview
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val list = ArrayList<Users> ()
+    val listUsers = arrayOf(
+            "Alpha",
+            "Bravo",
+            "Charlie",
+            "Delta",
+            "Echo",
+            "Foxtrot",
+            "Golf",
+            "Hotel",
+            "India",
+            "Juliet"
+
+    )
+
+    val halo = arrayOf( "halo"
+
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mRecyclerView.setHasFixedSize(true)
+        mRecyclerView.layoutManager = LinearLayoutManager (this)
+        for (i in 0 until listUsers.size) {
+            list.add(Users(listUsers.get(i)))
+            if(listUsers.size - 1 == i) {
+                val adapter = Adapter(list)
+                adapter.notifyDataSetChanged()
+                mRecyclerView.adapter = adapter
+                adapter.setOnClickListener {
+                    Toast.makeText(this, "Halo " + listUsers[it], Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        mRecyclerView.scrollToPosition(halo.size -1);
+    }
+}
